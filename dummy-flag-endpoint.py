2c6139b6171f7c769757a6509ca5ee9c1225292c
MMunier <marc@geomaps.de>
2021-10-31 13:24:47 +0100

    make flagsink a bit more talkative

--- dummy-flag-endpoint.py
+++ dummy-flag-endpoint.py
@@ -24,14 +24,18 @@ async def handle_flagsubmission(rx: asyncio.StreamReader, tx: asyncio.StreamWrit
                 break
             
             result_string = random.choice(responses)
+
+            print(f"[*] {line[:-1]} recieved returning {result_string}")
             tx.write(line[:-1] + b" " + result_string + b"\n")
             await tx.drain()
     except Exception as e:
-        print(e) 
+        print(f"[!] {e}") 
 
 
 async def main():
+    print("[*] Dummy Flagserver starting up!")
     server = await asyncio.start_server(handle_flagsubmission, "127.0.0.1", 1337)
+    print("[*] Flagserver serving on 127.0.0.1:1337\n")
     await server.serve_forever()
 
 asyncio.run(main())
